126ff6a3d25b3b2e880b5cabeca6b3e63bfd9e97
Javier Rojas <jaronu42@gmail.com>
2020-09-29 09:16:28 +0200

    Update clase_json.php

--- clase_json.php
+++ clase_json.php
@@ -4,44 +4,54 @@ class jsonUtil {
 
     public $dato;
 
+    public function isJson ($_dato = '')
+    {
 
-
-    public function isJson ($_dato = ''){
-
-        if ($_dato != '') $this->dato = $_dato;
+        if ($_dato != '')
+        { 
+            $this->dato = $_dato;
+        }
 
         $result = json_decode($this->dato);
 
-        if (json_last_error() === JSON_ERROR_NONE) {
-            
-            return true;
-
-        }else{
-
+        if (json_last_error_msg() === 'No error') 
+        {
+            return $result;
+        }
+        else
+        {
             return false;
-
-    }
+        }
     
-}
+    }
 
-public function codificaJson ($_dato = ''){
+    public function codificaJson ($_dato = '')
+    {
 
-    if ($_dato != '') $this->dato = $_dato;
+        if ($_dato != '')
+        {
+            $this->dato = $_dato;
+        }
 
         $this->dato = json_encode($this->dato, JSON_UNESCAPED_UNICODE);
+        
         return $this->dato;
-    
-}
+        
+    }
 
-public function decodificaJson ($_dato = '', $param = true){
-    
-    if ($_dato != '') $this->dato = $_dato;
-    $this->dato = json_decode($this->dato, $param);// true devuelve un array asiciativo al decodificar
-    
-  
-      
-      return $this->dato;
-    
-}
+    public function decodificaJson ($_dato = '', $param = true)
+    {
+        
+        if ($_dato != '')
+        {
+            $this->dato = $_dato;
+        }
+
+        // true devuelve un array asiciativo al decodificar
+        $this->dato = json_decode($this->dato, $param);
+        
+        return $this->dato;
+        
+    }
 
 }
